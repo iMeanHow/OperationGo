cdc5adccdf0fd3a4bfa63c27581fa037ad42aa50
mliu25 <mliu25@ncsu.edu>
2020-03-17 18:39:04 -0400

    +P/S exchange

--- mainPSConsume.go
+++ mainPSConsume.go
@@ -1,1 +1,8 @@
 package main
+
+import "HelloGo/OperationGo/RabbitMQ"
+
+func main() {
+	rabbitmq := RabbitMQ.NewRabbitMQPubSub("" + "newProduct")
+	rabbitmq.RecieveSub()
+}
--- mainPSPub.go
+++ mainPSPub.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"HelloGo/OperationGo/RabbitMQ"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	rabbitmq := RabbitMQ.NewRabbitMQPubSub("" + "newProduct")
+	for i := 0; i < 100; i++ {
+		rabbitmq.PublishPub("P/S published No." + strconv.Itoa(i) + " message")
+		fmt.Println("P/S produced No." + strconv.Itoa(i) + " message")
+		time.Sleep(1 * time.Second)
+	}
+}
